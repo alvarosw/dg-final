447ff74b23e7387c40db3b638f30915cd764e43f
alvarosw <alvarossmiguel@gmail.com>
2024-04-08 10:21:42 -0300

    feat: definição do root como /calculator

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def root_view(_):
+    return redirect('calculator/')
 
 urlpatterns = [
     path("api/", include('api.urls'), name='api'),
     path("admin/", admin.site.urls),
     path("consumer/", include('consumer.urls'), name='consumer'),
     path("calculator/", include('calculator.urls'), name='calculator'),
+
+    path("", root_view),
 ]
